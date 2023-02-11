132a1ad7b234a5ce0958099c6719b573bbec6e6f
Arya161 <arya.209303049@muj.manipal.edu>
2023-02-11 16:15:26 +0530

    Initial Commit

---
+++ success.php
@@ -0,0 +1,33 @@
+<html lang="en">
+<head>
+	<title></title>
+	<style>
+		@import url(//cdnjs.cloudflare.com/ajax/libs/normalize/3.0.1/normalize.min.css);
+		@import url(//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css);
+	</style>
+	<link rel="stylesheet" href="https://2-22-4-dot-lead-pages.appspot.com/static/lp918/min/default_thank_you.css">
+	<script src="https://2-22-4-dot-lead-pages.appspot.com/static/lp918/min/jquery-1.9.1.min.js"></script>
+	<script src="https://2-22-4-dot-lead-pages.appspot.com/static/lp918/min/html5shiv.js"></script>
+</head>
+<body>
+	<a href="index.php">Go Back To Coza Store</a>
+	<br>
+	<br>
+	<a href="https://docs.google.com/forms/d/e/1FAIpQLScmPge4X6419iPJhcFLHEs3Zk30EyhnHC-YmeHQdDRSF1X3Aw/viewform?usp=sf_link">Please Rate Us!</a>
+	<header class="site-header" id="header">
+		<h1 class="site-header__title" data-lead-id="site-header-title">THANK YOU!</h1>
+	</header>
+
+	<div class="main-content">
+		<i class="fa fa-check main-content__checkmark" id="checkmark"></i>
+		<p class="main-content__body" data-lead-id="main-content-body">Thanks a bunch for shopping with us. It means a lot to us, just like you do! We really appreciate you giving us a moment of your time today. Thanks for being you.</p>
+	</div>
+
+	<footer class="site-footer" id="footer">
+		<p class="site-footer__fineprint" id="fineprint">Copyright ©2014 | All Rights Reserved,Manipal University Jaipur.</p><br>
+    Contact for support - 6388****(Arya)
+	
+	</footer>
+	
+</body>
+</html>
